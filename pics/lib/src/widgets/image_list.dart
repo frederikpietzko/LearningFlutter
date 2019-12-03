4dc504f0b3f12bae5bebf88088dc87227030c93c
Frederik Pietzko <pietzkofred@gmail.com>
2019-12-03 12:26:10 +0100

    basic layout

--- pics/lib/src/widgets/image_list.dart
+++ pics/lib/src/widgets/image_list.dart
@@ -6,14 +6,31 @@ class ImageList extends StatelessWidget {
 
   ImageList(this.images);
 
+  Widget buildImage(ImageModel image) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      padding: EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+      ),
+      child: Column(
+        children: [
+          Padding(
+            child: Image.network(image.url),
+            padding: EdgeInsets.only(bottom: 10.0),
+          ),
+          Text(image.title),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: images.length,
       itemBuilder: (context, int index) {
-        return Image.network(
-          images[index].url,
-        );
+        return buildImage(images[index]);
       },
     );
   }
