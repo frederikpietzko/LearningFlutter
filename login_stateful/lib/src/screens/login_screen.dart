93ac2e571cd8a6abba84b6e2e6dd12da588d1d8d
Frederik Pietzko <pietzkofred@gmail.com>
2019-12-04 14:54:51 +0100

    simple form

--- login_stateful/lib/src/screens/login_screen.dart
+++ login_stateful/lib/src/screens/login_screen.dart
@@ -8,10 +8,46 @@ class LoginScreen extends StatefulWidget {
 }
 
 class LoginScreenState extends State<LoginScreen> {
+  Widget emailField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: "Email address",
+        hintText: "you@example.com",
+      ),
+      keyboardType: TextInputType.emailAddress,
+    );
+  }
+
+  Widget passwordField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        hintText: 'Password',
+        labelText: 'Password',
+      ),
+      obscureText: true,
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      onPressed: () => print("Test"),
+      child: Text("Submit"),
+      color: Colors.blue,
+    );
+  }
+
   Widget build(context) {
     return Container(
-      margin: EdgeInsets.all(20.0),
-      child: Text("LoginScreen"),
-    );
+        margin: EdgeInsets.all(20.0),
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              emailField(),
+              passwordField(),
+              Container(margin: EdgeInsets.only(top: 25.0)),
+              submitButton(),
+            ],
+          ),
+        ));
   }
 }
